690ae5fa60bba0cc7206500868dae53ef8fde5eb
pawangupta5167 <52108857+pawangupta5167@users.noreply.github.com>
2022-01-10 18:25:40 +0530

    12. Column name from a given column number

---
+++ Amazon/column_name_from_given_column_number.cpp
@@ -0,0 +1,15 @@
+//https://practice.geeksforgeeks.org/problems/column-name-from-a-given-column-number4244/1/
+
+class Solution {
+public:
+    string colName (long long int n)
+    {
+        string temp="";
+        while(n>0){
+            char ch='A'+(n-1)%26;
+            temp=ch+temp;
+            n=(n-1)/26;
+        }
+        return temp;
+    }
+};
