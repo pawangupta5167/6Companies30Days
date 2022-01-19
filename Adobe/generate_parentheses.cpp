4055c61ff22761577170abe36bc36a3003f8cca8
pawangupta5167 <52108857+pawangupta5167@users.noreply.github.com>
2022-01-19 12:26:09 +0530

    6. Generate all possible parentheses

---
+++ Adobe/generate_parentheses.cpp
@@ -0,0 +1,123 @@
+//https://practice.geeksforgeeks.org/problems/generate-all-possible-parentheses/1/#
+
+// { Driver Code Starts
+
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> AllParenthesis(int n) ;
+
+ // } Driver Code Ends
+
+//User function Template for C++
+
+// N is the number of pairs of parentheses
+
+// Return list of all combinations of balanced parantheses
+
+class Solution
+
+{
+
+    public:
+
+    void solve(vector<string>&vec, string op, int open, int close)
+
+   {
+
+       if(open==0 && close==0)
+
+       {
+
+           vec.push_back(op);
+
+           return;
+
+       }
+
+       if(open!=0)
+
+       {
+
+           string op1=op;
+
+           op1.push_back('(');
+
+           solve(vec,op1,open-1,close);
+
+       }
+
+       if(close>open)
+
+       {
+
+           string op1=op;
+
+           op1.push_back(')');
+
+           solve(vec,op1,open,close-1);
+
+       }
+
+   }
+
+   vector<string> AllParenthesis(int n) 
+
+   {
+
+       // Your code goes here
+
+       vector<string>vec;
+
+       string op;
+
+       int open=n;
+
+       int close=n;
+
+       solve(vec,op,open,close);
+
+       return vec;
+
+    }
+
+};
+
+// { Driver Code Starts.
+
+int main() 
+
+{ 
+
+	int t;
+
+	cin>>t;
+
+	while(t--)
+
+	{
+
+		int n;
+
+		cin>>n;
+
+		Solution ob;
+
+		vector<string> result = ob.AllParenthesis(n); 
+
+		sort(result.begin(),result.end());
+
+		for (int i = 0; i < result.size(); ++i)
+
+			cout<<result[i]<<"\n";
+
+	}
+
+	return 0; 
+
+} 
+
+  // } Driver Code Ends
