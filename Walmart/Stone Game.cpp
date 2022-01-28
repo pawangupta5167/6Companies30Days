dfc9d7a73acbf264297bf4a45fccf1c1bdcf240b
pawangupta5167 <52108857+pawangupta5167@users.noreply.github.com>
2022-01-28 12:31:46 +0530

    2. Stone Game

---
+++ Walmart/Stone Game.cpp
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/stone-game/description/
+
+class Solution {
+
+public:
+
+    bool stoneGame(vector<int>& piles) {
+
+        if (piles.size() % 2 == 0 || piles.size() == 1) {
+
+            return true;
+
+        }
+
+        vector<int> dp(piles.size());
+
+        for (int i = piles.size() - 1; i >= 0; --i) {
+
+            dp[i] = piles[i];
+
+            for (int j = i + 1; j < piles.size(); ++j) {
+
+                dp[j] = max(piles[i] - dp[j], piles[j] - dp[j - 1]);
+
+            }
+
+        }
+
+        
+
+        return dp.back() >= 0;
+
+    }
+
+};
